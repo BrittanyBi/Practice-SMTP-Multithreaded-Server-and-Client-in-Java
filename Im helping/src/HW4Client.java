e017625b322f911225c081ac97186de9de83b723
Brittany <biancobrittany@yahoo.com>
2018-02-25 15:58:12 -0700

    Updated for v0.3

--- Im helping/src/HW4Client.java
+++ Im helping/src/HW4Client.java
@@ -43,7 +43,7 @@ public class HW4Client {
             System.err.println("Couldn't get I/O for the connection to: "  + hostName);
             System.exit(1);
         }
-        /*
+        
         // Prompt user for sender's email address, recipient's email address, subject, and email contents:
         System.out.println("Please input the sender's email address:");
         String sender = sysIn.readLine();
@@ -54,37 +54,34 @@ public class HW4Client {
         System.out.println("Please input the email subject:");
         String subject = sysIn.readLine();
         
-        System.out.println("Please input the email message. " +
-                           "\nEnd input with a single period alone on a line ("."):");
+        System.out.println("Please input the email message. "
+                         + "\nEnd input with a single period alone on a line (\".\"):");
         
-        String message = null,
+        String message = "",
                body = "";
-        while (!(message = sysIn.readLine()).equals(".")) {
+        while (!message.equals(".")) {
+           message = sysIn.readLine();
            body += message;  // double-check whether .nextLine() strips off newlines
         }
-        */
+        
+        System.out.println("All input accepted.");
+        
         // implement steps 4 & 5.
-
-        String fromServer;
-        String fromUser;
-
-        while ((fromUser = sysIn.readLine()) != null) {
-		      System.out.println("Client: " + fromUser);
-            socketOut.println(fromUser);
-				
-				if ((fromServer = socketIn.readLine()) != null)
-				{
-					System.out.println("Server: " + fromServer);
-				}
-				else {
-                System.out.println("Server replies nothing!");
-                break;
-				}
-		    
-			   if (fromUser.equals("Bye."))
-					break;
-         
-        }
+        socketOut.println("HELO bb.com");
+        System.out.println(socketIn.readLine());
+        
+        socketOut.println("MAIL FROM: " + sender);
+        System.out.println(socketIn.readLine());
+        
+        socketOut.println("RCPT TO: " + recipient);
+        System.out.println(socketIn.readLine());
+        
+        socketOut.println("Subject line: " + subject);
+        System.out.println(socketIn.readLine());
+        
+        socketOut.println("<Mail message>\n.\n");
+        System.out.println(socketIn.readLine());
+        
 
         socketOut.close();
         socketIn.close();
