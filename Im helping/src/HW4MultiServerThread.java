2187eb49ad5b7b354fd0ed0131aca22c01ba5a09
Brittany <biancobrittany@yahoo.com>
2018-02-25 13:31:01 -0700

    Add files via upload

--- Im helping/src/HW4MultiServerThread.java
+++ Im helping/src/HW4MultiServerThread.java
@@ -8,20 +8,20 @@ import java.net.*;
 import java.io.*;
 
 public class HW4MultiServerThread extends Thread {
-    private Socket clientTCPSocket = null;
+   private Socket clientTCPSocket = null;
+   PrintWriter cSocketOut = null;
+   BufferedReader cSocketIn = null;
 
-    public HW4MultiServerThread(Socket socket) {
+   public HW4MultiServerThread(Socket socket) {
 		super("HW4MultiServerThread");
 		clientTCPSocket = socket;
-    }
+   }
 
-    public void run() {
+   public void run() {
 
 		try {
-	 	   PrintWriter cSocketOut = new PrintWriter(clientTCPSocket.getOutputStream(), true);
-	  		BufferedReader cSocketIn = new BufferedReader(
-				    new InputStreamReader(
-				    clientTCPSocket.getInputStream()));
+	 	   cSocketOut = new PrintWriter(clientTCPSocket.getOutputStream(), true);
+	  		cSocketIn = new BufferedReader(new InputStreamReader(clientTCPSocket.getInputStream()));
                 
          System.out.println("Thread started. Writing to client...");
          
@@ -31,6 +31,9 @@ public class HW4MultiServerThread extends Thread {
          cSocketOut.println(address.toString());
          
          System.out.println("Client contacted.");
+         
+         System.out.println("Starting state machine...");
+         stateMachine(1);
          
 	      String fromClient, toClient;
 			  
@@ -52,5 +55,94 @@ public class HW4MultiServerThread extends Thread {
 		} catch (IOException e) {
 		    e.printStackTrace();
 		}
-    }
+   }
+   public void stateMachine(int state) {
+      String fromClient;
+      
+      
+      // wait for, read, & display each:
+      switch (state) {
+         case 0:    // received QUIT
+            
+            cSocketOut.println("221 <server’s ip> closing connection");
+            break;
+            
+            
+         case 1:     // waiting for HELO
+            while (!(fromClient = cSocketIn.readLine()).equals("HELO") || !(fromClient = cSocketIn.readLine()).equals("QUIT")) {
+               cSocketOut.println("503 5.5.2 Send hello first");
+            }
+            
+            // state transitions
+            if (fromClient.equals("HELO")) {
+               state = 2;
+               cSocketOut.println("250 <server’s ip> Hello <client’s ip>");
+               
+            } else if (fromClient.equals("QUIT")) {
+               state = 0;
+            }
+            
+            break;
+            
+            
+         case 2:     // waiting for MAIL FROM
+            while (!(fromClient = cSocketIn.readLine()).equals("MAIL FROM") || !(fromClient = cSocketIn.readLine()).equals("QUIT")) {
+               cSocketOut.println("503 5.5.2 Need mail command");
+            }
+            
+            // state transitions
+            if (fromClient.equals("MAIL FROM")) {
+               state = 3;
+               cSocketOut.println("250 2.1.0 Sender OK");
+               
+            } else if (fromClient.equals("QUIT")) {
+               state = 0;
+            }
+            
+            break;
+            
+            
+         case 3:     // waiting for RCPT TO
+            while (!(fromClient = cSocketIn.readLine()).equals("RCPT TO") || !(fromClient = cSocketIn.readLine()).equals("QUIT")) {
+               cSocketOut.println("503 5.5.2 Need rcpt command");
+            }
+            
+            // state transitions
+            if (fromClient.equals("RCPT TO")) {
+               state = 4;
+               cSocketOut.println("250 2.1.5 Recipient OK");
+               
+            } else if (fromClient.equals("QUIT")) {
+               state = 0;
+            }
+            
+            break;
+            
+            
+         case 4:     // waiting for DATA
+            while (!(fromClient = cSocketIn.readLine()).equals("DATA") || !(fromClient = cSocketIn.readLine()).equals("QUIT")) {
+               cSocketOut.println("503 5.5.2 Need data command");
+            }
+            
+            // state transitions
+            if (fromClient.equals("DATA")) {
+               state = 5;
+               cSocketOut.println("354 Start mail input; end with <CRLF>.<CRLF>");
+               
+            } else if (fromClient.equals("QUIT")) {
+               state = 0;
+            }
+            
+            break;
+            
+            
+         case 5:     // waiting for full email message
+            cSocketOut.println("250 Message received and to be delivered");
+            break;
+            
+            
+         default:    // error encountered.
+            return;
+      }
+   }
 }
